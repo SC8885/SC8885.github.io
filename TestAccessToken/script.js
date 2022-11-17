b72ceb0bac0c553e0758ba46a363a0f7f4e64d46
Shubhangi Chavan <75863488+SC8885@users.noreply.github.com>
2022-11-17 09:29:11 -0500

    Update script.js

--- TestAccessToken/script.js
+++ TestAccessToken/script.js
@@ -2,56 +2,31 @@ function showAlert(message) {
     alert(message)
 }
 
+function getTeamsContext() {
+  microsoftTeams.app.getContext().then(async (context) =>{
+    
+  });
+}
+
 function initializeMST(message) {
-    microsoftTeams.app.initialize().then(() => {
-        microsoftTeams.app.getContext().then(async (context) => {
-          // Generate random state string and store it, so we can verify it in the callback
-          var currentURL = new URL(window.location);
-          var clientId = currentURL.searchParams.get("clientId");
-          var scope = currentURL.searchParams.get("scope");
-          var loginHint = currentURL.searchParams.get("loginHint");
+  microsoftTeams.app.initialize().then(() => {
+    microsoftTeams.app.getContext().then(async (context) => {
+      alert(context.user.tenant.id)
+    });
 
-          const msalConfig = {
-            auth: {
-              clientId: clientId,
-              authority: `https://login.microsoftonline.com/${context.user.tenant.id}`,
-              navigateToLoginRequestUrl: false
-            },
-            cache: {
-              cacheLocation: "sessionStorage",
-            },
-          }
+    var authTokenRequest = {
+        successCallback: function (result) {
+            //call server side to exchange the  token from teams with access token & used it to call graph
+            fetch("/token?token=" + result)              
+                        .then(json => json.json()).then(result=> {
+                          alert(context.user.tenant.id);
+                          console.log(result);
+                        }); 
+          
+        },
+        failureCallback: function (error) { console.log("Failure: " + error); },
+      };
 
-          alert(context.user.tenant.id)
-
-          var authTokenRequest = {
-              successCallback: function (result) {
-                  //call server side to exchange the  token from teams with access token & used it to call graph
-                  fetch("/token?token=" + result)              
-                              .then(json => json.json()).then(result=> {
-                                  document.getElementById("jsonData").textContent = JSON.stringify(result, undefined, 2);
-                              });
-                            
-                
-              },
-              failureCallback: function (error) { console.log("Failure: " + error); },
-          };
-          microsoftTeams.authentication.getAuthToken(authTokenRequest)
-              .then((response) => {
-                if (!response) {
-                    console.error('GetToken failed');
-                    reject();
-                    return;
-                }
-                console.log('resolve(response)', response);
-                  alert(response)
-                  resolve(response);
-              })
-              .catch((error) => {
-                console.error('GetToken failed', error);
-                alert(error);
-                reject();
-              });
-        });
-      });
+      microsoftTeams.authentication.getAuthToken(authTokenRequest);
+  });
 }
