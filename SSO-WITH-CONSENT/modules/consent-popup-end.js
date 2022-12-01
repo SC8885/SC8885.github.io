a54fbf19b7a20a88651097a315d53424d91e1785
Shubhangi Chavan <75863488+SC8885@users.noreply.github.com>
2022-12-01 11:09:24 -0500

    Removed unused projects

--- SSO-WITH-CONSENT/modules/consent-popup-end.js
+++
@@ -1,6 +0,0 @@
-microsoftTeams.initialize();
-
-microsoftTeams.getContext((context) => {
-    // Just close the popup, there's nothing to send back to the tab
-    microsoftTeams.authentication.notifySuccess();
-});
