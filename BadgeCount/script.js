8059d28c1af8f05a4c3fd1c5ce90a34887c7a705
Shubhangi Chavan <75863488+SC8885@users.noreply.github.com>
2023-02-28 14:30:10 -0500

    Add files via upload

---
+++ BadgeCount/script.js
@@ -0,0 +1,26 @@
+
+// Get a client side token from Teams
+async function getTeamsToken() {
+  console.log("getting TeamsToken");
+  await microsoftTeams.initialize();
+  microsoftTeams.authentication.getAuthToken({
+      successCallback: (result) => {
+          console.log("TeamsToken successCallback");
+          window.rflxMediator('getTeamsToken', result);
+      },
+      failureCallback: (error) => {
+          console.log("TeamsToken failureCallback" + error);
+          window.rflxMediator('getTeamsToken', null);
+      }
+  });
+}
+
+// Get Teams context
+async function getTeamsContext() {
+  console.log("getting TeamsContext");
+  await microsoftTeams.initialize();
+  microsoftTeams.getContext((context) => {
+      console.log("TeamsContext successCallback");
+      window.rflxMediator('getTeamsContext', JSON.stringify(context));
+  });
+}
