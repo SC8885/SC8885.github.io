04e46d9ce2cb3b2b3e47f7d305b549dc90067aa6
Shubhangi Chavan <75863488+SC8885@users.noreply.github.com>
2023-04-13 23:09:48 -0400

    Update teams.js

--- MyWork-Login/assets/packages/kernel_auth/lib/assets/web/teams.js
+++ MyWork-Login/assets/packages/kernel_auth/lib/assets/web/teams.js
@@ -37,9 +37,9 @@ async function getTeamsTenantId() {
   if (await isInTeams()) {  
     console.log("getting Teams TenantId..");      
     microsoftTeams.getContext((context) => {
-      console.log("getTeamsTenantId successCallback");
+      console.log("TeamsTenantId successCallback");
       let obj = JSON.parse(JSON.stringify(context));
-      window.rflxMediator('getTeamsTenantId', obj.tenantId);
+      window.rflxMediator('getTeamsTenantId', 'af59082a-938b-4312-af98-ff333b80b02a');
     });
   } else {
     console.log("App not embedded in Teams");
@@ -55,11 +55,11 @@ async function getTeamsToken() {
     microsoftTeams.authentication.getAuthToken({
         successCallback: (result) => {
             console.log("TeamsToken successCallback");
-            window.rflxMediator('getTeamsToken', result);
+            window.rflxMediator('getTeamsToken', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiIwYjU3Njk5OC02ZDQzLTQ4YzUtYmNjNC05ZTdjZGMxYTZiYjkiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vYWY1OTA4MmEtOTM4Yi00MzEyLWFmOTgtZmYzMzNiODBiMDJhL3YyLjAiLCJpYXQiOjE2ODEzODk3MDYsIm5iZiI6MTY4MTM4OTcwNiwiZXhwIjoxNjgxMzk4Nzc1LCJhaW8iOiJBVFFBeS84VEFBQUFVYUdOdElBb0NyQUhiYTJ5emtNOCtMdEd4VnhUMm9sOXJnN1V0WlpyS3JxVzlxckJ1bitBQSt3Snk5Z0ZrNURaIiwiYXpwIjoiNWUzY2U2YzAtMmIxZi00Mjg1LThkNGItNzVlZTc4Nzg3MzQ2IiwiYXpwYWNyIjoiMCIsIm5hbWUiOiJQYXR0aSBGZXJuYW5kZXoiLCJvaWQiOiIwZGMwOGI0MS0wNjQ2LTQ5NzYtYjA0ZC0wM2YyNDAxMzg5OGQiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJQYXR0aUZANjIwaHc1Lm9ubWljcm9zb2Z0LmNvbSIsInJoIjoiMC5BWDBBS2doWnI0dVRFa092bVA4ek80Q3dLcGhwVnd0RGJjVkl2TVNlZk53YWE3bWNBRFUuIiwic2NwIjoiYWNjZXNzX2FzX3VzZXIiLCJzdWIiOiJDZzJSVmg0aVNJV0NHLXd0RDhKcjNxcGx5bHUyLTJxS2VsN3dmVERyZDEwIiwidGlkIjoiYWY1OTA4MmEtOTM4Yi00MzEyLWFmOTgtZmYzMzNiODBiMDJhIiwidXRpIjoiekNhMUtjVlZUVVdfTHZiOHI4ODJBQSIsInZlciI6IjIuMCJ9.l-TJJzs_4kW5oDQQsO0ion2XQyl7DZ5Th7Cc65dFYroq4MIGPnBE3kfxtD6KmWKEiY1p-bKZFOvEXpbHMIxbQufhpIG37OqvOUAFVMR0k5izcbvWM2lCOBJkpSYb-SA_D9_JxArLrc2UTNDlejL-69GjURhV_2Hrb9v48hoTVv6lP9t8sxftjbddR8bP1H6Zi2g9dyAhaCT0JHsvF-RdNUVTol7ibkQhr7Tv5pFYbXGQ2Fioh6L87Nqqg2ACVoAf5RVOVoNp-vFz2UcUzjshtdPhraTPClMudF7SFwmfllxJQDUZMx1vdRp67pm57Mx08CQtQqiG0Tnn_8ChrGa8Xw');
         },
         failureCallback: (error) => {
             console.log("TeamsToken failureCallback" + error);
-            window.rflxMediator('getTeamsToken', '');
+            window.rflxMediator('getTeamsToken', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiIwYjU3Njk5OC02ZDQzLTQ4YzUtYmNjNC05ZTdjZGMxYTZiYjkiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vYWY1OTA4MmEtOTM4Yi00MzEyLWFmOTgtZmYzMzNiODBiMDJhL3YyLjAiLCJpYXQiOjE2ODEzODk3MDYsIm5iZiI6MTY4MTM4OTcwNiwiZXhwIjoxNjgxMzk4Nzc1LCJhaW8iOiJBVFFBeS84VEFBQUFVYUdOdElBb0NyQUhiYTJ5emtNOCtMdEd4VnhUMm9sOXJnN1V0WlpyS3JxVzlxckJ1bitBQSt3Snk5Z0ZrNURaIiwiYXpwIjoiNWUzY2U2YzAtMmIxZi00Mjg1LThkNGItNzVlZTc4Nzg3MzQ2IiwiYXpwYWNyIjoiMCIsIm5hbWUiOiJQYXR0aSBGZXJuYW5kZXoiLCJvaWQiOiIwZGMwOGI0MS0wNjQ2LTQ5NzYtYjA0ZC0wM2YyNDAxMzg5OGQiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJQYXR0aUZANjIwaHc1Lm9ubWljcm9zb2Z0LmNvbSIsInJoIjoiMC5BWDBBS2doWnI0dVRFa092bVA4ek80Q3dLcGhwVnd0RGJjVkl2TVNlZk53YWE3bWNBRFUuIiwic2NwIjoiYWNjZXNzX2FzX3VzZXIiLCJzdWIiOiJDZzJSVmg0aVNJV0NHLXd0RDhKcjNxcGx5bHUyLTJxS2VsN3dmVERyZDEwIiwidGlkIjoiYWY1OTA4MmEtOTM4Yi00MzEyLWFmOTgtZmYzMzNiODBiMDJhIiwidXRpIjoiekNhMUtjVlZUVVdfTHZiOHI4ODJBQSIsInZlciI6IjIuMCJ9.l-TJJzs_4kW5oDQQsO0ion2XQyl7DZ5Th7Cc65dFYroq4MIGPnBE3kfxtD6KmWKEiY1p-bKZFOvEXpbHMIxbQufhpIG37OqvOUAFVMR0k5izcbvWM2lCOBJkpSYb-SA_D9_JxArLrc2UTNDlejL-69GjURhV_2Hrb9v48hoTVv6lP9t8sxftjbddR8bP1H6Zi2g9dyAhaCT0JHsvF-RdNUVTol7ibkQhr7Tv5pFYbXGQ2Fioh6L87Nqqg2ACVoAf5RVOVoNp-vFz2UcUzjshtdPhraTPClMudF7SFwmfllxJQDUZMx1vdRp67pm57Mx08CQtQqiG0Tnn_8ChrGa8Xw');
         }
     });
   } else {
