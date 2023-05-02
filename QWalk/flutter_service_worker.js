12f7ab8727262fbd0b93a8e581acc0d1df789e48
Shubhangi Chavan <75863488+SC8885@users.noreply.github.com>
2023-05-02 14:04:21 -0400

    QWalk updated

--- QWalk/flutter_service_worker.js
+++ QWalk/flutter_service_worker.js
@@ -4,10 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "7d3ca817fc942c196bc0aa27fc464658",
-"index.html": "7e435ad12410ba4e4347ce246da79a13",
+"index.html": "76613561646e3e2bd58428dbd3e3191b",
 "/": "ad05061631025637ee5b1cb5f26cf4ca",
-"main.dart.js": "c6bec88a008e3b02974299a3c03f8596",
-"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
+"main.dart.js": "ff92949722991c612c81572506d09495",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -195,11 +194,7 @@ const RESOURCES = {
 "assets/packages/kernel_auth/lib/assets/web/teams.js": "0c2133157df934ce0d5757db654f1280",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
 "assets/assets/config.json": "79b3afda14b14621589ac559af43631a",
-"assets/assets/colors.json": "1fdf3e03428d7b8b205caa1c489f815e",
-"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
-"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
-"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
-"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e"
+"assets/assets/colors.json": "1fdf3e03428d7b8b205caa1c489f815e"
 };
 
 // The application shell files that are downloaded before a service worker can
